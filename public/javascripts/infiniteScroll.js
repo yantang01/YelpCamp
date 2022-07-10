2cf9e28f13d3eb4bf96c418684541feb6d9e9f05
Yan Tang <92637601+yantang01@users.noreply.github.com>
2022-07-09 20:27:35 -0400

    make it better

--- public/javascripts/infiniteScroll.js
+++
@@ -1,7 +0,0 @@
-const body = document.querySelector("#infinite");
-
-body.onscroll = function () {
-    if (window.scrollY > (document.body.offsetHeight - window.outerHeight)) {
-        body.style.height = document.body.offsetHeight + 100 + "px";
-    }
-}
